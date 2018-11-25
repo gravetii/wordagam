8e0e3777573c41b28a9161b86cb8b224906dcede
Sandeep Dasika <dasika.sandy@gmail.com>
2018-11-25 23:46:05 +0530

    Refresh existing game grid if any upon theme change

--- fx/src/io/github/gravetii/scene/EditThemeScene.java
+++ fx/src/io/github/gravetii/scene/EditThemeScene.java
@@ -1,5 +1,6 @@
 package io.github.gravetii.scene;
 
+import io.github.gravetii.theme.ThemeService;
 import javafx.scene.Scene;
 import javafx.scene.layout.GridPane;
 import javafx.stage.Stage;
@@ -8,6 +9,14 @@ public class EditThemeScene extends FxScene {
 
   public EditThemeScene(Stage stage) {
     super(stage);
+    Stage parent = (Stage) stage.getOwner();
+    stage.setOnCloseRequest(
+        event -> {
+          GridPane gridPane = (GridPane) parent.getScene().lookup("#gridPane");
+          if (gridPane != null) {
+            gridPane.setBackground(new ThemeService().current().getGameGridBackground());
+          }
+        });
   }
 
   @Override
