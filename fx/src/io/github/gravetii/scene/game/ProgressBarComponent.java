9788ed415f387e3cfb61f4db6100972347b4634c
Sandeep Dasika <dasika.sandy@gmail.com>
2018-12-15 23:34:17 +0530

    CSS changes

--- fx/src/io/github/gravetii/scene/game/ProgressBarComponent.java
+++ fx/src/io/github/gravetii/scene/game/ProgressBarComponent.java
@@ -1,32 +1,30 @@
 package io.github.gravetii.scene.game;
 
-import io.github.gravetii.controller.GameTimerTask;
 import io.github.gravetii.controller.ProgressBarController;
 import io.github.gravetii.scene.FxComponent;
 import io.github.gravetii.store.Settings;
-import io.github.gravetii.util.SingleLatestTaskScheduler;
 import javafx.scene.control.ProgressBar;
 import javafx.scene.layout.BorderPane;
 
 public class ProgressBarComponent extends FxComponent<ProgressBarController, ProgressBar> {
 
-  private GameTimerTask task;
+  private BorderPane root;
 
   public ProgressBarComponent(BorderPane root) throws Exception {
     super("progressBar.fxml");
-    double gameTime = Settings.getGameTime();
-    this.task = new GameTimerTask(root, gameTime);
-    SingleLatestTaskScheduler.get().submit(task);
+    this.root = root;
     this.create();
   }
 
   @Override
   protected ProgressBarController createController() {
-    return new ProgressBarController(this.task);
+    double gameTime = Settings.getGameTime();
+    return new ProgressBarController(this.root);
   }
 
   @Override
   protected ProgressBar createNode() throws Exception {
     return (ProgressBar) this.loadNode();
   }
+
 }
