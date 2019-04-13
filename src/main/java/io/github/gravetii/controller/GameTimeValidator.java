142d45fe6ec5b4b25ee2c63eee9bba24dd6c7582
Sandeep Dasika <dasika.sandy@gmail.com>
2019-04-13 14:42:56 +0530

    Minor refactoring

--- src/main/java/io/github/gravetii/controller/GameTimeValidator.java
+++ src/main/java/io/github/gravetii/controller/GameTimeValidator.java
@@ -14,61 +14,61 @@ public class GameTimeValidator {
 
   private ValidationRule<Pair<String, String>, Pair<String, String>> nonEmptyRule() {
     return obj -> {
-      if (obj != null) {
-        String min = obj.getKey().isEmpty() ? "0" : obj.getKey();
-        String sec = obj.getValue().isEmpty() ? "0" : obj.getValue();
-        return new Pair<>(min, sec);
+      if (obj == null) {
+        return null;
       }
 
-      return null;
+      String min = obj.getKey().isEmpty() ? "0" : obj.getKey();
+      String sec = obj.getValue().isEmpty() ? "0" : obj.getValue();
+      return new Pair<>(min, sec);
     };
   }
 
   private ValidationRule<Pair<String, String>, Pair<String, String>> isNumberRule() {
     return obj -> {
-      if (obj != null) {
-        String min = obj.getKey();
-        String sec = obj.getValue();
-        return (min.matches("[0-9]+") && sec.matches("[0-9]+")) ? obj : null;
+      if (obj == null) {
+        return null;
       }
 
-      return null;
+      String min = obj.getKey();
+      String sec = obj.getValue();
+      return (min.matches("[0-9]+") && sec.matches("[0-9]+")) ? obj : null;
     };
   }
 
   private ValidationRule<Pair<String, String>, Pair<Integer, Integer>> integerParser() {
     return obj -> {
-      if (obj != null) {
-        try {
-          int minutes = Integer.parseInt(obj.getKey());
-          int seconds = Integer.parseInt(obj.getValue());
-          return new Pair<>(minutes, seconds);
-        } catch (NumberFormatException e) {
-          return null;
-        }
+      if (obj == null) {
+        return null;
       }
 
-      return null;
+      try {
+        int minutes = Integer.parseInt(obj.getKey());
+        int seconds = Integer.parseInt(obj.getValue());
+        return new Pair<>(minutes, seconds);
+      } catch (NumberFormatException e) {
+        return null;
+      }
     };
   }
 
   private ValidationRule<Pair<Integer, Integer>, GameTime> gameTimeParser() {
     return obj -> {
-      if (obj != null) {
-        int minutes = obj.getKey();
-        int seconds = obj.getValue();
-        if (minutes == 0 && seconds == 0) {
-          return null;
-        }
-
-        if (seconds < 0 || seconds > 60) {
-          return null;
-        }
-
-        return new GameTime(minutes, seconds);
+      if (obj == null) {
+        return null;
       }
 
-      return null;
+      int minutes = obj.getKey();
+      int seconds = obj.getValue();
+      if (minutes == 0 && seconds == 0) {
+        return null;
+      }
+
+      if (seconds < 0 || seconds > 60) {
+        return null;
+      }
+
+      return new GameTime(minutes, seconds);
     };
   }
 }
