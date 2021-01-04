669d52da5a696242f661480ce5232458ce12dea8
Sandeep Dasika <dasika.sandy@gmail.com>
2021-01-04 19:32:47 +0530

    Adding module-info.java

---
+++ src/main/java/module-info.java
@@ -0,0 +1,37 @@
+module io.github.gravetii.wordagam {
+  requires javafx.base;
+  requires javafx.controls;
+  requires javafx.fxml;
+  requires javafx.graphics;
+  requires java.logging;
+  requires java.prefs;
+  requires java.base;
+
+  opens io.github.gravetii.game;
+  opens io.github.gravetii.scene;
+  opens io.github.gravetii.scene.start;
+  opens io.github.gravetii.scene.game;
+  opens io.github.gravetii.scene.help;
+  opens io.github.gravetii.scene.menu;
+  opens io.github.gravetii.scene.settings;
+  opens io.github.gravetii.scene.theme;
+  opens io.github.gravetii.controller;
+  opens io.github.gravetii.theme;
+
+  exports io.github.gravetii;
+  exports io.github.gravetii.scene;
+  exports io.github.gravetii.scene.game;
+  exports io.github.gravetii.scene.help;
+  exports io.github.gravetii.scene.start;
+  exports io.github.gravetii.scene.menu;
+  exports io.github.gravetii.scene.settings;
+  exports io.github.gravetii.scene.theme;
+  exports io.github.gravetii.controller;
+  exports io.github.gravetii.db;
+  exports io.github.gravetii.dictionary;
+  exports io.github.gravetii.game;
+  exports io.github.gravetii.model;
+  exports io.github.gravetii.theme;
+  exports io.github.gravetii.validation;
+  exports io.github.gravetii.util;
+}
